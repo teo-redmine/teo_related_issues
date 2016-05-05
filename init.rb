caed63f918a0509f67a0db8fb16272d54100c645
Juan Leal <juan.leal@juntadeandalucia.es>
2016-05-05 15:28:22 +0200

    Estructura básica creada por rake

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :teo_related_issues do
+  name 'Teo Related Issues plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
